f7e2dd1b5cc3d7c57b50f0a5107d8d649d59d1d9
jvargh <joji_varghese@hotmail.com>
2024-01-07 15:22:08 -0500

    mods. added sql

--- aks_infra/main.tf
+++ aks_infra/main.tf
@@ -41,54 +41,54 @@ resource "azurerm_container_registry" "acr" {
 data "azurerm_client_config" "current" {
 }
 
-data "azurerm_user_assigned_identity" "identity" {
-  name                = "${azurerm_kubernetes_cluster.k8s.name}-agentpool"
-  resource_group_name = azurerm_kubernetes_cluster.k8s.node_resource_group 
+# data "azurerm_user_assigned_identity" "identity" {
+#   name                = "${azurerm_kubernetes_cluster.k8s.name}-agentpool"
+#   resource_group_name = azurerm_kubernetes_cluster.k8s.node_resource_group 
+# }
+
+# resource "azurerm_role_assignment" "role_acrpull" {
+#   scope                            = azurerm_container_registry.acr.id
+#   role_definition_name             = "AcrPull"
+#   principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
+#   skip_service_principal_aad_check = true
+# }
+
+resource "azurerm_sql_server" "sql" {
+  name                         = var.sql_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.db_admin_login
+  administrator_login_password = var.db_admin_password
 }
 
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
-  skip_service_principal_aad_check = true
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-# resource "azurerm_sql_server" "sql" {
-#   name                         = var.sql_name
-#   resource_group_name          = azurerm_resource_group.rg.name
-#   location                     = azurerm_resource_group.rg.location
-#   version                      = "12.0"
-#   administrator_login          = var.db_admin_login
-#   administrator_login_password = var.db_admin_password
-# }
+resource "azurerm_sql_database" "db" {
+  name                = var.db_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sql.name
+  create_mode         = "Default"
+  edition             = "Standard"
 
-# resource "azurerm_storage_account" "storage" {
-#   name                     = var.storage_name
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+  tags = {
+    environment = "production"
+  }
+}
 
-# resource "azurerm_sql_database" "db" {
-#   name                = var.db_name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = azurerm_resource_group.rg.location
-#   server_name         = azurerm_sql_server.sql.name
-#   create_mode         = "Default"
-#   edition             = "Standard"
-
-#   tags = {
-#     environment = "production"
-#   }
-# }
-
-# resource "azurerm_sql_firewall_rule" "rule" {
-#   name                = "AllowAzureServicesAndResources"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   server_name         = azurerm_sql_server.sql.name
-#   # The Azure feature Allow access to Azure services can be enabled 
-#   # by setting start_ip_address and end_ip_address to 0.0.0.0
-#   start_ip_address = "0.0.0.0"
-#   end_ip_address   = "0.0.0.0"
-# }
+resource "azurerm_sql_firewall_rule" "rule" {
+  name                = "AllowAzureServicesAndResources"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.sql.name
+  # The Azure feature Allow access to Azure services can be enabled 
+  # by setting start_ip_address and end_ip_address to 0.0.0.0
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
