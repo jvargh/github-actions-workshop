eef35ed5d9e33cc4f095a18a7dc7ebdfab3fd463
jvargh <joji_varghese@hotmail.com>
2024-01-07 17:30:35 -0500

    mods 530

--- aks_infra/main.tf
+++ aks_infra/main.tf
@@ -41,17 +41,17 @@ resource "azurerm_container_registry" "acr" {
 data "azurerm_client_config" "current" {
 }
 
-# data "azurerm_user_assigned_identity" "identity" {
-#   name                = "${azurerm_kubernetes_cluster.k8s.name}-agentpool"
-#   resource_group_name = azurerm_kubernetes_cluster.k8s.node_resource_group 
-# }
+data "azurerm_user_assigned_identity" "identity" {
+  name                = "${azurerm_kubernetes_cluster.k8s.name}-agentpool"
+  resource_group_name = azurerm_kubernetes_cluster.k8s.node_resource_group 
+}
 
-# resource "azurerm_role_assignment" "role_acrpull" {
-#   scope                            = azurerm_container_registry.acr.id
-#   role_definition_name             = "AcrPull"
-#   principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
-#   skip_service_principal_aad_check = true
-# }
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = data.azurerm_user_assigned_identity.identity.principal_id
+  skip_service_principal_aad_check = true
+}
 
 # resource "azurerm_sql_server" "sql" {
 #   name                         = var.sql_name
